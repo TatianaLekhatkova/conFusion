76c015c2a413044837967e03d25b1128e7cc2c6f
Tatiana <tlekhatkova@gmail.com>
2020-06-02 16:45:37 +0200

    Template-driven Forms

--- Documents/program/Angular/conFusion/src/app/app.module.ts
+++ Documents/program/Angular/conFusion/src/app/app.module.ts
@@ -17,11 +17,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import {AppRoutingModule} from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService} from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -49,6 +53,10 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatButtonModule,
     AppRoutingModule,
     MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [
     DishService,
